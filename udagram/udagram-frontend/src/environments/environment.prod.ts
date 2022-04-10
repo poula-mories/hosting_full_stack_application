7da66fe846a632ce1cd37fc07830e8b4b9d3fae5
Poula Mories <poulamories5@gmail.com>
2022-04-10 20:39:22 +0200

    apiHost updated

--- udagram/udagram-frontend/src/environments/environment.prod.ts
+++ udagram/udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: true,
   appName: "Udagram",
-  apiHost: "http://udagram-dev.eba-ykhyvjaa.us-east-1.elasticbeanstalk.com/api/v0",
+  apiHost: "http://udagramenv-env.eba-iexns6c2.us-east-1.elasticbeanstalk.com/api/v0",
 };
 
 /*
